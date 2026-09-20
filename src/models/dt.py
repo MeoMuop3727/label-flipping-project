fbf4c8f8a7805a1c134e70534c8da960b50a28c3
Hua Nguyen Khoi <memuopmapdit1234@gmail.com>
2026-09-20 21:08:38 +0800

    refactor: adding scaler for two models

--- src/models/dt.py
+++ src/models/dt.py
@@ -1,10 +1,15 @@
 from ..preprocess import train_dfs
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
 
 dt_trained = {}
 
 for flip_percent, data in train_dfs.items():
     X_train, y_train = data["X"], data["attack"]
+
+    X_train = scaler.fit_transform(X_train)
 
     dt = DecisionTreeClassifier()
     dt.fit(X_train, y_train)
--- src/models/lr.py
+++ src/models/lr.py
@@ -1,10 +1,15 @@
 from ..preprocess import train_dfs
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
 
 lr_trained = {}
 
 for flip_percent, data in train_dfs.items():
     X_train, y_train = data["X"], data["attack"]
+
+    X_train = scaler.fit_transform(X_train)
 
     lr = LogisticRegression()
     lr.fit(X_train, y_train)
