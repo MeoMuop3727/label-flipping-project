4fa18382b0d702b097712fcde9cfa8e0ad2170d4
Hua Nguyen Khoi <memuopmapdit1234@gmail.com>
2026-09-23 23:47:53 +0800

    feat: adding new defend, clf.py\n-updating version, v1.2.1

--- src/defend/__init__.py
+++ src/defend/__init__.py
@@ -1,4 +1,5 @@
 from .regularization_sweep import regularization_sweep
 from .self_cleaning import cleaning
+from .cfl import cv_filter
 
-__all__ = ["regularization_sweep", "cleaning"]
+__all__ = ["regularization_sweep", "cleaning", "cv_filter"]
